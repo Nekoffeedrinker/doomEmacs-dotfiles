bac05fee85ad77e40f9b618654c2ca63be76ce27
Denis Castillón <orangeneko158@outlook.com>
2026-08-09 14:34:16 -0700

    traducir parte del init.el
    Como la configuración en el init.el no se recomienda hacerle tangle,
    decidí mantenerlo por separado teniéndolo lo más limpio y bonito
    posible. Eso incluye el mejorar los comentarios, por lo que aquí los
    empiezo a traducir.

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -1,21 +1,21 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
-;; This file controls what Doom modules are enabled and what order they load
-;; in. Remember to run 'doom sync' after modifying it!
+;; Este archivo controla qué modulos de Doom son habilitados y en qué
+;; orden se cargan. Recuerda usar 'doom sync' después de modificar
 
-;; NOTE: Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
-;;   documentation. There you'll find a link to Doom's Module Index where all of
-;;   our modules are listed, including what flags they support.
+;; NOTE: Usa 'SPC h d h' (o 'C-h d h') para acceder a la documentación
+;;   de Doom. Ahí podrás encontrar un enlace al índice de módulos donde
+;;   todos los módulos están listados, incluyendo qué /flags/ soportan.
 
-;; NOTE: Move your cursor over a module's name (or its flags) and press 'K' (or
-;;   'C-c c k' for non-vim users) to view its documentation. This works on flags
-;;   as well (those symbols that start with a plus).
+;; NOTE: Mueve el cursor sobre el nombre de un módulo (o su flag) y
+;;   presiona K (o C-c c k) para ver su documentación. Esto funciona
+;;   en flags también (los símbolos que inician con el signo más).
 ;;
-;;   Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
-;;   directory (for easy access to its source code).
+;;   Alternativamente, presiona 'gd' (o 'C-c c gd') en un módulo para
+;;   buscar su directorio (para acceso fácil a su código fuente).
 
 (doom! :input
-       ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
+       ;;bidi              ; Habilitar escritura derecha a izquierda
        ;;chinese
        ;;japanese
        ;;layout            ; auie,ctsrnm is the superior home row
